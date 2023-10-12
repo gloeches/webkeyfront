5f9f6b5629be5892ab076cd78aab958517ed9363
gloeches <gloeches@gmail.com>
2023-10-12 19:53:38 +0200

    updated constant

--- src/app/shared/header/constants.ts
+++ src/app/shared/header/constants.ts
@@ -1,5 +1,5 @@
-//export const rootUrl = 'http://poseidon.spain.agilent.com:8080';
-export const rootUrl = 'http://springboot.local:8080';
+export const rootUrl = 'http://poseidon.spain.agilent.com:8080';
+//export const rootUrl = 'http://springboot.local:8080';
 export const authUrl = "/api/v1/auth";
 export const apiUrl= "/api/v1"
 
